35aabbe9136b599431bbe24adb4975ee52b3dc82
gabriel-castro <gabrielcastro.developer@gmail.com>
2024-03-06 00:25:37 -0300

    initial setup and environment variables

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('the app is working');
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+const app = express();
+
+// Environment variables
+const PORT = process.env.PORT;
+
+// Middlewere
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    console.log('Request recieved sucessfully');
+    res.send('Hello, world');
+})
+
+app.listen(PORT, () => {
+    console.log('The app is running on port ' + PORT);
+});
