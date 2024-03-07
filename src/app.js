edd96dce42a230add6484421ac30cf26e86b549a
gabriel-castro <gabrielcastro.developer@gmail.com>
2024-03-07 15:21:31 -0300

    importing session variables

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ import cors from 'cors';
 import express from 'express';
 const app = express();
 import routes from './routes/routes.js';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 // Middlewere
 app.use(cors());
+app.use(cookieParser('my_cookie_secret'));
+app.use(
+    session({
+      secret: "my_session_secret",
+      resave: false,
+      saveUninitialized: true,
+      cookie: false
+    })
+);
 app.use(express.json());
 app.use('/api', routes);
 
